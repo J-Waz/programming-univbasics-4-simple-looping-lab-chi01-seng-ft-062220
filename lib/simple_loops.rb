1ece38086d313f44a7b240d0b276a7f14df8565d
Jacob G. Wazydrag <jgw.jakegeorge@gmail.com>
2020-06-14 17:13:40 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,34 +1,48 @@
-# Write your methods here
-
-def loop_message_five_times(message)
+def loop_message_five_times(aMessageString)
   count = 0 
   while count < 5 do 
-    puts message
-    count += 1
+    puts aMessageString
+  count += 1
   end
+end 
+loop_message_five_times("A String")
+
+
+def loop_message_n_times(aMessageString, integer)
+  count = 0 
+  while count < integer
+  puts aMessageString
+  count += 1
+end
 end
 
-def loop_message_n_times(message, limit)
-  count = 0 
-  while count < limit do
-    puts message 
-    count += 1 
-  end
-end
+loop_message_n_times("I control the number of times this puts!", 3)
 
 def output_array(array)
-  count = 0 
-  while count < array.length do 
-    puts array[count]
-    count += 1 
+  counter = 0
+  while counter < array.length
+    puts array[counter]
+    counter += 1
   end
+end
+output_array(["John","Jacob", "Mom"])
+
+def return_string_array(array)
+ count = 0
+  new_array = []
+  while count < array.length do
+   new_array.push(array[count].to_s) # OR new_array << array[count].to_s
+    count += 1
+  end
+  new_array
 end
 
 def return_string_array(array)
-  count = 0
-  while count < array.length do 
- array[count] = array[count].to_s
-  count += 1
+  count = 0 
+  new_array = []
+    while count < array.length do 
+  new_array << array[count].to_s
+  count += 1 
   end
-  array
+  p new_array
 end
